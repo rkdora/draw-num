2473bfc5edc92f838679802b6224c889f23702e9
ryuto <rryuto5210@gmail.com>
2019-12-18 14:50:18 +0900

    Add empty-images.sh

---
+++ empty-images.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf static/images/
+mkdir static/images/
+mkdir -p static/images/pure/
+mkdir -p static/images/noise/
+mkdir -p static/images/inverse/
+mkdir -p static/images/adv/
+touch static/images/pure/.gitkeep
+touch static/images/noise/.gitkeep
+touch static/images/inverse/.gitkeep
+touch static/images/adv/.gitkeep
